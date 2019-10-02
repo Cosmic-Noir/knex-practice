88be2c9c6811fb1d1c98f620765434b1fc6baff5
Kristen Doucette <cosmicnoir90@gmail.com>
2019-10-02 12:56:49 -0600

    Add articles-service.js

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,23 @@
+INSERT INTO blogful_articles (title, date_published, content) 
+VALUES 
+    ('How to Win', now() - '1 days'::INTERVAL, 'To win you must be strong and do stuff...'),
+    ('How to Lose', now() - '2 days'::INTERVAL, 'To lost you must be weak and not do stuff...'),
+    ('How to Love', now() - '3 days'::INTERVAL, 'To love you must be honest and open.'),
+    ('How to Survive', now() - '4 days'::INTERVAL, 'To survive you must be crafty and creative'),
+    ('How to Forage', now() - '5 days'::INTERVAL, 'You had better be ready to eat some berries'),
+    ('Shopping On A Budget', now() - '6 days'::INTERVAL, 'Use a spreadsheet and caclcuate your budget'),
+    ('Shaping The Blog', now() - '7 days'::INTERVAL, 'The blog will grow and grow forever.'),
+    ('Brian Goes West', now() - '8 days'::INTERVAL, 'It was a dusty day while the sun set in the west...'),
+    ('Le Marriage', now() - '9 days'::INTERVAL, 'Once there was love, then there was marriage...'),
+    ('When in Rome', now() - '10 days'::INTERVAL, 'Do as the Romans do.'),
+    ('The Final Fronteir', now() - '11 days'::INTERVAL, 'Against a vast, black sky...'),
+    ('How to Become a Web Dev', now() - '12 days'::INTERVAL, 'Time to put on your big-boy pants'),
+    ('Someone Likes You', now() - '13 days'::INTERVAL, 'That is right! Someone out there likes you.'),
+    ('Uh Oh Spaghetti Os', now() - '14 days'::INTERVAL, 'Now made with real sauce!'),
+    ('Up To No Good', now() - '15 days'::INTERVAL, 'There was a dangerous gleam in his eyes...'),
+    ('Strength - Your Choice', now() - '16 days'::INTERVAL, 'Pick yourself up in 30 days or less!'),
+    ('Winning The Lottery', now() - '17 days'::INTERVAL, 'Now that you have boatloads of money, it is time to go shopping!'),
+    ('Shopping Without A Budget', now() - '18 days'::INTERVAL, 'Go for it and buy all the things you want!'),
+    ('How to Build a Server from Scratch', now() - '19 days'::INTERVAL, 'Start by making some basic files, and then use Express to...'),
+    ('Dodging Bullets', now() - '20 days'::INTERVAL, 'Escape the rain of gun-fire by following these simple tips...')
+
