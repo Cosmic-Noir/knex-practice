99e0f7b8eb7eabd64dd436f70b604c956b99040a
Kristen Doucette <cosmicnoir90@gmail.com>
2019-10-02 10:18:22 -0600

    Success connecting to knex-practice database with knex

--- src/practice.js
+++ src/practice.js
@@ -1,6 +1,16 @@
+require("dotenv").config();
 const knex = require("knex");
 
 const knexInstance = knex({
   client: "pg",
   connection: process.env.DB_URL
 });
+
+knexInstance
+  .from("amazong_products")
+  .select("*")
+  .then(result => {
+    console.log(result);
+  });
+
+// console.log("knex and driver installed correctly");
